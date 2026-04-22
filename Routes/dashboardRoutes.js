be9e3fc6dd55f157b4c5d0784b49f5f526f0893b
Tusiime Parvin Ruth <parvin.t.ruth@gmail.com>
2026-04-23 00:16:58 +0300

    Edited some files

--- Routes/dashboardRoutes.js
+++ Routes/dashboardRoutes.js
@@ -1,19 +1,28 @@
-const express = require ("express");
+const express = require("express");
 // const router = express.Router();
 
 const router = express.Router();
-const { isSystemAdmin, isParkingAttendant, isSectionManager } = require("../middleware/auth");
+// const { isSystemAdmin, isParkingAttendant, isSectionManager } = require("../middleware/auth");
 
-router.get("/adminDashboard", isSystemAdmin, (req, res) => {
-    res.render("adminReports");
-});
+router.get(
+  "/adminDashboard",
+  /*isSystemAdmin,*/ (req, res) => {
+    res.render("adminDashboard");
+  },
+);
 
-router.get("/attendantDashboard", isParkingAttendant, (req, res) => {
+router.get(
+  "/attendantDashboard",
+  /*isParkingAttendant,*/ (req, res) => {
     res.render("attendantDashboard");
-});
+  },
+);
 
-router.get("/managerDashboard", isSectionManager, (req, res) => {
-  res.render("manager-dashboard");
-});
+router.get(
+  "/managerDashboard",
+  /*isSectionManager,*/ (req, res) => {
+    res.render("managerDashboard");
+  },
+);
 
 module.exports = router;
